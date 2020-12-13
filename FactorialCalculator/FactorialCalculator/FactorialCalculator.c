f047c229cf707c847443497588daf2d34a994c1c
Krisz Kaszas <kriszrokakaszas@gmail.com>
2020-12-13 14:44:26 +0100

    done

---
+++ FactorialCalculator/FactorialCalculator/FactorialCalculator.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf_s("Calculating 8!:\n\n");
+	int iterator = 1;
+	int sequenceSum = 1;
+	while (iterator <= 8)
+	{
+		sequenceSum *= iterator;
+		printf_s("%d! = %d\n", iterator, sequenceSum);
+		iterator++;
+	}
+	return 0;
+}
